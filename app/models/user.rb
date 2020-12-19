6c79b90d95c3c98f35fd13d7a07e1469f883bf17
Ubuntu <ubuntu@ip-172-31-4-179.ap-northeast-1.compute.internal>
2020-12-19 16:07:12 +0000

    ユーザー認証機能を追加

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+    has_secure_password
+    
+    validates :name,
+        presence: true, #必須項目
+        uniqueness: true, #重複するIDなし
+        length: { maximum: 32 },
+        format: {
+            with: /\A[a-z0-9_]+\z/,
+            message: 'は小文字英数字,アンダーバーで入力してください'
+        }
+    validates :password,
+        length: { minimum: 8 }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  get'mypage', to:'users#me'#mypageにアクセスでuserコントローラのmeが呼ばれる
+  
+  post 'login', to: 'sessions#create'
+  get 'auth', to: 'users#auth'
+  delete 'logout', to:'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'home#index' 
+  resources :users, only: %i[new create]
 end
