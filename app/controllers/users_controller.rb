cb88b4c0b7fa1f25a1bf203a0fdca338fe803083
Ubuntu <ubuntu@ip-172-31-4-179.ap-northeast-1.compute.internal>
2020-12-20 15:06:40 +0000

    認証周りのURLを変更した

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id;
-      redirect_to mypage_path
+    user = User.new(user_params) #ユーザを入力された情報をもとに作成
+    if user.save #userを正常に登録できた場合
+      session[:user_id] = user.id; #セッションにuser_idを保存
+      redirect_to mypage_path #mypageへリダイレクト
     else
       redirect_back fallback_location: root_path, flash: {
         user: user,
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
       }
     end
   end
-  def auth 
+  def login
     
   end
   def me
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
-  get'mypage', to:'users#me'#mypageにアクセスでuserコントローラのmeが呼ばれる
+  get 'eatery', to:'eateries#index'#飲食店一覧ページ
+  get 'find', to: 'eateries#find'#飲食店を探すページ
+  get 'share', to: 'eateries#share'#飲食店を登録するページ
   
-  post 'login', to: 'sessions#create'
-  get 'auth', to: 'users#auth'
+  get 'mypage', to:'users#me'#mypageにアクセスでuserコントローラのmeが呼ばれる
+  get 'signup', to:'users#new'
+  get 'login', to: 'users#login'
+  
+  post 'authenticated', to: 'sessions#create'
+  
   delete 'logout', to:'sessions#destroy'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index' 
   resources :users, only: %i[new create]
