d4429434fd9d127baf90c29ecde06198c059eaad
Ubuntu <ubuntu@ip-172-31-4-179.ap-northeast-1.compute.internal>
2020-12-21 07:43:40 +0000

    飲食店を登録する機能を実装、また飲食店を一覧表示する機能を追加

--- app/controllers/eateries_controller.rb
+++ app/controllers/eateries_controller.rb
@@ -1,7 +1,25 @@
 class EateriesController < ApplicationController
     def index
+        @eatery = Eatery.all
     end
     def new
         @eatery = Eatery.new
     end
+    def create
+        eatery = Eatery.create(eatery_params)
+        if eatery.save 
+          redirect_to eateries_path
+        else
+          redirect_back fallback_location: root_path, flash: {
+            eatery: eatery
+          }
+        end
+    end
+    
+    private
+    
+    def eatery_params
+        params.require(:eatery).permit(:name, :addres, nil, nil, :category, :payment, :parking)
+    end
+        
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
   #eateiesコントローラ
-  get 'eatery', to:'eateries#index'#飲食店一覧ページ
+  get 'eateries', to:'eateries#index'#飲食店一覧ページ
   get 'find', to: 'eateries#find'#飲食店を探すページ
-  get 'reg', to: 'eateries#new'#飲食店を登録するページ
-  
+  get 'eateries/new', to: 'eateries#new'#飲食店を登録するページ
+  post 'eateries', to:'eateries#create'
   #usersコントローラ　
   get 'mypage', to:'users#me'#mypageにアクセスでuserコントローラのmeが呼ばれる
   get 'signup', to:'users#new'
-  get 'login', to: 'users#auth'
+  
   
   #sessionsコントローラ 主にログイン状態の保持のために使用
+  get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to:'sessions#destroy'
   
