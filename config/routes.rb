b0d67e905045824e3b51b5547306be4fb3636d51
Ubuntu <ubuntu@ip-172-31-4-179.ap-northeast-1.compute.internal>
2020-12-25 15:59:31 +0000

    routesのcategoryは使っていなかったので削除

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   
   get 'eateries', to:'eateries#index'#飲食店一覧ページ
   get 'eateries/new', to: 'eateries#new'#飲食店を登録するページ
-  get 'eateries/recent', to: 'eateries#recent'
-  get 'eateries/category', to: 'eateries#category'
+  get 'eateries/recent', to: 'eateries#recent'#最新のお店を取得する
+  #get 'eateries/category', to: 'eateries#category'
   get 'eateries/search', to: 'eateries#search'
   post 'eateries', to:'eateries#create'
   get 'eateries/:id', to: 'eateries#show'
