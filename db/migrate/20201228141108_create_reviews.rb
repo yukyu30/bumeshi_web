78d797283d0975caf7c45009c95061fa7b7c1afa
Ubuntu <ubuntu@ip-172-31-4-179.ap-northeast-1.compute.internal>
2021-01-01 12:40:05 +0000

    uuidへの変更に伴い、マイグレーションファイルを変更

--- db/migrate/20201228141108_create_reviews.rb
+++ db/migrate/20201228141108_create_reviews.rb
@@ -1,8 +1,8 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
-    create_table :reviews do |t|
-      t.references :user, null: false, foreign_key: true
-      t.references :eatery, null: false, foreign_key: true
+    create_table :reviews, id: :uuid do |t|
+      t.references :user, type: :uuid, null: false, foreign_key: true
+      t.references :eatery, type: :uuid, null: false, foreign_key: true
       t.integer :price, null: false
       t.integer :rate, null: false
       t.text :comment, null: false
