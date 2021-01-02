42b2ef2e4be188c35d24da0aa8ab60ab3bdd1e9c
Ubuntu <ubuntu@ip-172-31-4-179.ap-northeast-1.compute.internal>
2021-01-02 12:47:33 +0000

    間違った関数名を修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def new
     @auth = request.env["omniauth.auth"]
     @user = User.find_by(provider: @auth.provider, uid: @auth.uid) #ユーザーの認証
-    if @user.preset? 
+    if @user.present? 
       redirect_to create_session_path #userが存在するのでログイン処理へ
     else
       redirect_to new_user_path
