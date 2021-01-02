0a6edad4ccc666fcdc27e05fdbd9742744f2f01c
Ubuntu <ubuntu@ip-172-31-4-179.ap-northeast-1.compute.internal>
2021-01-02 17:45:55 +0000

    users登録の流れを変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def signin
-    @omiauth = save_auth_info(request.env["omniauth.auth"]) #googleから返された情報を格納
+    @omiauth = User.save_auth_info(request.env["omniauth.auth"]) #googleから返された情報を格納
     user = User.find_by(provider: @omiauth.provider, uid: @omiauth.uid)
     if user.present?
       session[:user_id] = user.id
@@ -34,14 +34,4 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name).(uid: @auth.uid, image: @auth.image, oauth_token: @auth.credentials.token, oauth_expires_at: Time.at(@auth.credentials.expires_at))
   end
   
-  def save_auth_info(@)
-    @omiauth = {
-      provider: auth.provider,
-      uid: auth.uid,
-      name:  auth.info.name,
-      image: auth.info.image,
-      oauth_token: auth.credentials.token,
-      oauth_expires_at: Time.at(auth.credentials.expires_at)
-    }
-  end
 end
