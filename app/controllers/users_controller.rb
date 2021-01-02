8696158cf756c730640d2e0d8ef345cda1c8f234
Ubuntu <ubuntu@ip-172-31-4-179.ap-northeast-1.compute.internal>
2021-01-02 16:42:29 +0000

    ユーザー名を任意の文字列に設定できるように変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   end
   
   def new
-    @user = User.new(flash[:user]);
+    
   end
 
   def create
--- db/migrate/20210102101656_create_users.rb
+++ db/migrate/20210102101656_create_users.rb
@@ -1,12 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users, id: :uuid do |t|
-      t.string :provider
-      t.string :uid
-      t.string :name
-      t.string :image
-      t.string :oauth_token
-      t.datetime :oauth_expires_at
+      t.string :provider, null: false
+      t.string :uid, null: false
+      t.string :name, null: false
+      t.string :image,null: false
+      t.string :oauth_token, null:false
+      t.datetime :oauth_expires_at, null:false
 
       t.timestamps
     end
