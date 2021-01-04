1ce89461200a9dfd626b3538ea1733905777710b
Ubuntu <ubuntu@ip-172-31-4-179.ap-northeast-1.compute.internal>
2021-01-04 08:17:19 +0000

    必要なCSSが削除されてしまったのでビルドをし直した

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   purge: {
     enabled: true,
-    content:['./app/views/**/*.html.erb']
+    content:[
+      './app/views/**/*.html.erb',
+      './app/views/layouts/header/*.html.erb',
+      './app/views/layouts/header/parts/*.html.erb'
+      
+    ]
   },
   
   darkMode: false, // or 'media' or 'class'
